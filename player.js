2b3577156a6f3f3b418b38209e7289ee0095576f
Travis CI User <travis@example.org>
2018-10-16 12:53:36 +0000

    Build for 41cb7c724c5cb3816bb42e2c430ddaf994efb5a3

--- player.js
+++ player.js
@@ -1,7 +1,7 @@
 var GUI =
 (window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([[4],{
 
-/***/ 1589:
+/***/ 1591:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -51,7 +51,7 @@ var _titledHoc2 = _interopRequireDefault(_titledHoc);
 
 var _mode = __webpack_require__(116);
 
-var _player = __webpack_require__(1590);
+var _player = __webpack_require__(1592);
 
 var _player2 = _interopRequireDefault(_player);
 
@@ -122,11 +122,11 @@ _reactDom2.default.render(_react2.default.createElement(WrappedPlayer, { isPlaye
 
 /***/ }),
 
-/***/ 1590:
+/***/ 1592:
 /***/ (function(module, exports, __webpack_require__) {
 
 
-var content = __webpack_require__(1591);
+var content = __webpack_require__(1593);
 
 if(typeof content === 'string') content = [[module.i, content, '']];
 
@@ -148,7 +148,7 @@ if(false) {}
 
 /***/ }),
 
-/***/ 1591:
+/***/ 1593:
 /***/ (function(module, exports, __webpack_require__) {
 
 exports = module.exports = __webpack_require__(4)(false);
@@ -166,5 +166,5 @@ exports.locals = {
 
 /***/ })
 
-},[[1589,0]]]);
+},[[1591,0]]]);
 //# sourceMappingURL=player.js.map
